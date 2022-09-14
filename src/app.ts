5cff3926c713cafcd078b233aa5da9d2418a8833
Guilherme <guilhermeevencioluz@gmail.com>
2022-09-14 18:54:29 -0300

    feat: Adds MongoModel file to the project

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import errorHandler from './middlewares/ErrorHandler';
+import route from './routes/cars.routes';
 
 const app = express();
+
+app.use(route);
+app.use(errorHandler);
 
 export default app;
---
+++ src/middlewares/ErrorHandler.ts
@@ -0,0 +1,17 @@
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+
+const errorHandler: ErrorRequestHandler = (
+  err: Error | ZodError,
+  _req,
+  res,
+  _next,
+) => {
+  if (err instanceof ZodError) {
+    return res.status(400).json({ message: err.issues });
+  }
+
+  return res.status(500).json({ message: 'internal error' });
+};
+
+export default errorHandler;
