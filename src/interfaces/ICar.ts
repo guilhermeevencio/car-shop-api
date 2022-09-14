3eb3f11b92bc91adec2d29bd9e6b74eca9a6b156
Guilherme <guilhermeevencioluz@gmail.com>
2022-09-14 15:57:36 -0300

    feat: create interface ICar.

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import vehicleZodSchema from './IVehicle';
+
+const carZodSchema = z.intersection(vehicleZodSchema, z.object({
+  doorsQty: z.number().int().min(2).max(4),
+  seatsQty: z.number().int().min(2).max(7),
+}));
+
+export type ICar = z.infer<typeof carZodSchema>;
