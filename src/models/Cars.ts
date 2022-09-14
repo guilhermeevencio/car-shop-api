0d0c4d1f1de91fffa51f74f639ada7ca19674c2a
Guilherme <guilhermeevencioluz@gmail.com>
2022-09-14 19:05:05 -0300

    feat: Adds Car model to the project.

---
+++ src/models/Cars.ts
@@ -0,0 +1,19 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const carsMongooseSchema = new Schema<ICar>({
+  buyValue: Number,
+  color: String,
+  doorsQty: Number,
+  model: String,
+  seatsQty: Number,
+  status: Boolean,
+  year: Number,
+});
+
+export default class Cars extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('Cars', carsMongooseSchema)) {
+    super(model);
+  }
+}
