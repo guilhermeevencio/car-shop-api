b3549e52f2ab5e8ba60b049409ff270a819b0dc6
Guilherme <guilhermeevencioluz@gmail.com>
2022-09-15 16:04:23 -0300

    feat: Add type IMotorcycle

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import vehicleZodSchema from './IVehicle';
+
+const motorcycleZodSchema = z.intersection(vehicleZodSchema, z.object({
+  category: z.union([z.literal('Street'), z.literal('Custom'), z.literal('Trail')]),
+  engineCapacity: z.number().int().min(0).max(2500),
+}));
+
+export type IMotorcycle = z.infer<typeof motorcycleZodSchema>;
---
+++ src/routes/motorcycles.routes.ts
@@ -0,0 +1,5 @@
+import { Router, Request, Response } from 'express';
+
+const morotcycleRoutes = Router();
+
+export default morotcycleRoutes;
